8345854b5a79044552cbdef3f497a109b2647891
ohkm9926 <ohkm9926@naver.com>
2024-07-23 11:16:16 +0900

    react공부

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React,{Component} from 'react';
+import Customer from './components/Customer';
+import './App.css';
+import  {Table}  from '@material-ui/core';
+import {TableBody} from '@material-ui/core';
+import {TableHead} from '@material-ui/core';
+import { TableRow } from '@material-ui/core';
+import {TableCell} from '@material-ui/core';
+
+
+const customers =[
+  {
+  'id'   : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '오경민',
+  'birthday' : '961222',
+  'gender' :  '남자',
+  'job' : '개발자'
+},
+{
+  'id'   : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '오세민',
+  'birthday' : '950618',
+  'gender' :  '남자',
+  'job' : '대학생'
+},
+{
+  'id'   : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '오질섭',
+  'birthday' : '720415',
+  'gender' :  '남자',
+  'job' : '소매업자'
+}]
+
+function App () {
+  
   return (
-    <div className="gray-background">
-     <img src={logo} lat="logo"></img>
-     <h2>Let's develop management system!</h2>
-    </div>
+ 
+   <div>
+     <table>
+     <TableBody>
+    {
+   customers.map( c=> {
+    return(
+      <Customer 
+      key={c.id}
+      id={c.id}
+      image={c.image}
+      name={c.name}
+      birthday={c.birthday}
+      gender={c.gender}
+      job={c.job}
+      />
+    )
+   })
+  }
+  </TableBody>
+  </table>
+   </div>
+  
+    
   );
+
 }
 
 export default App;
